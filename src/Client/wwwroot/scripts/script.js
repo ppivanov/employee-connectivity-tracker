5a349947924ea4882901dc8771bcd4515b5c3e3e
Pavel Ivanov <ppivanov98@gmail.com>
2020-11-28 16:23:12 +0000

    Adding side menu

--- src/Client/wwwroot/scripts/script.js
+++ src/Client/wwwroot/scripts/script.js
@@ -6,3 +6,11 @@ const toggleUserDropdown = () => {
     let element = document.getElementById('dropdown')
     element.classList.toggle('display-none')
 }
+
+const openNav = () => {
+    document.getElementById('mySidenav').style.width = '20em'
+}
+
+const closeNav = () => {
+    document.getElementById('mySidenav').style.width = '0'
+}
