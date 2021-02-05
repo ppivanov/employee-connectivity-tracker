fd7eae219417fa6733391da96c1e74c7b711f7ff
Pavel Ivanov <ppivanov98@gmail.com>
2021-02-05 13:48:58 +0000

    Adding controller to handle comm weight reads and writes. #9

---
+++ src/Server/Controllers/CommunicationWeightsController.cs
@@ -0,0 +1,40 @@
+﻿using EctBlazorApp.Server.AuthorizationAttributes;
+using EctBlazorApp.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EctBlazorApp.Server.Controllers
+{
+    [Route("api/communication")]
+    [ApiController]
+    [AuthorizeAdmin]
+    public class CommunicationWeightsController : ControllerBase
+    {
+
+        private readonly EctDbContext _dbContext;
+
+        public CommunicationWeightsController(EctDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Route("weights")]
+        [HttpGet]
+        public IEnumerable<CommunicationWeight> GetWeights()
+        {
+            return _dbContext.CommunicationWeights;
+        }
+
+        [Route("weights/update")]
+        [HttpPut]
+        public async Task UpdateWeights([FromBody] IEnumerable<CommunicationWeight> weights)
+        {
+            _dbContext.CommunicationWeights.RemoveRange(_dbContext.CommunicationWeights);       // Delete all existing records.
+
+            _dbContext.CommunicationWeights.AddRange(weights);                                  // Add the updated list.
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
