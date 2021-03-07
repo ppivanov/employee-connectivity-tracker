6028b67a4dd19eebee83df773dd92716b5232d78
Pavel Ivanov <ppivanov98@gmail.com>
2021-03-07 23:27:36 +0000

    Adding basic move and remove actions to MoveMembers.razor #28

--- src/Client/Pages/MoveMembers.razor.cs
+++ src/Client/Pages/MoveMembers.razor.cs
@@ -17,8 +17,37 @@ namespace EctBlazorApp.Client.Pages
 
         protected List<EctTeam> InAppTeams { get; set; }
 
-        protected EctTeam LeftTeam { get; set; }
-        protected EctTeam RightTeam { get; set; }
+        protected string LeftTeamSelection { get; set; }
+        protected string RightTeamSelection { get; set; }
+
+        protected EctTeam LeftTeam
+        {
+            get
+            {
+                if (InAppTeams == null || string.IsNullOrWhiteSpace(LeftTeamSelection)) return null;
+                return InAppTeams.FirstOrDefault(t => t.Name.ToLower().Equals(LeftTeamSelection.ToLower()));
+            }
+        }
+        protected EctTeam RightTeam
+        {
+            get
+            {
+                if (InAppTeams == null || string.IsNullOrWhiteSpace(RightTeamSelection)) return null;
+                return InAppTeams.FirstOrDefault(t => t.Name.ToLower().Equals(RightTeamSelection.ToLower()));
+            }
+        }
+
+        protected void RemoveMember(EctTeam team, string emailToRemove)
+        {
+            team.Members = team.Members.Where(m => m.Email.Equals(emailToRemove) == false).ToList();
+        }
+
+        protected void MoveMember(EctTeam fromTeam, EctTeam toTeam, string emailToRemove)
+        {
+            var userToMove = fromTeam.Members.FirstOrDefault(m => m.Email.Equals(emailToRemove));
+            fromTeam.Members = fromTeam.Members.Where(m => m != userToMove).ToList();
+            toTeam.Members.Add(userToMove);
+        }
 
         protected override async Task OnInitializedAsync()
         {
