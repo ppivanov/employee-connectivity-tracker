ppivanov/employee-connectivity-tracker

commit 3d6f5a2ed90b18ca9b28a59e0a5c4a053c13f77b

Adding error message for invalid Team lead selection - ManageTeam

src/Client/Pages/ManageTeam.razor
@@ -54,7 +54,7 @@
                                 <label>Member selection</label>
                                 <input id="createTeamMember" list="members"
                                     @onchange="@((args) => SetMemberNameEmail(args))"
-                                    placeholder="Full Name <email@example.com>" style="@MemmberInputStyle" />
+                                    placeholder="Full Name <email@example.com>" style="@MemberInputStyle" />
                                 <datalist id="members">
                                     @foreach (var member in AvailableMembers)
                                     {

src/Client/Pages/ManageTeam.razor.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using static EctBlazorApp.Shared.SharedMethods;
@@ -27,7 +26,7 @@ namespace EctBlazorApp.Client.Pages
         protected string CurrentMemberSelection { get; set; } = "";
         protected bool HasTeamId { get => string.IsNullOrEmpty(HashedTeamId) == false; }
         protected string LeaderInputStyle { get => leaderInputError ? "border: 1px solid red" : ""; }
-        protected string MemmberInputStyle { get => memberInputError ? "border: 1px solid red" : ""; }
+        protected string MemberInputStyle { get => memberInputError ? "border: 1px solid red" : ""; }
         protected string ServerMessage { get; set; } = "";
         protected string ServerMessageInlineStyle
         {
@@ -92,10 +91,10 @@ namespace EctBlazorApp.Client.Pages
         protected async Task AddSelectedMember()
         {
             if (IsCurrentMemberSelectionEmpty()
-                || IsCurrentMemberSelectionFormatted(out string selectedEmail)
+                || IsCurrentMemberSelectionBadlyFormatted(out string selectedEmail)
                 || IsCurrentMemberSelectionAlreadyLeader(selectedEmail)
                 || IsCurrentMemberSelectionAlreadySelected(selectedEmail)
-                || IsCurrentMemberSelectionEligible(selectedEmail))
+                || IsCurrentMemberSelectionIneligible(selectedEmail))
                 return;
 
             teamDetails.MemberNamesAndEmails.Add(CurrentMemberSelection);
@@ -144,7 +143,11 @@ namespace EctBlazorApp.Client.Pages
         {
             if (isSubmitting
                 || AreTeamDetailsValid() == false
-                || IsLeaderAlreadySelectedAsMember())
+                || string.IsNullOrWhiteSpace(teamDetails.LeaderNameAndEmail)
+                || IsLeaderAlreadySelectedAsMember()
+                || IsCurrentLeaderSelectionBadlyFormatted(out string leaderEmail)
+                || IsCurrentLeaderSelectionIneligible(leaderEmail)
+                )
                 return;
 
             isSubmitting = true;
@@ -249,17 +252,32 @@ namespace EctBlazorApp.Client.Pages
             return false;
         }
 
-        private bool IsCurrentMemberSelectionFormatted(out string selectedEmail)
+        private bool IsCurrentLeaderSelectionBadlyFormatted(out string leaderEmail)
         {
-            if (IsStringInMemberFormat(CurrentMemberSelection) == false)
-            {
+            bool result = IsInputBadlyFormatted(teamDetails.LeaderNameAndEmail, out leaderEmail);
+            if (result)
+                leaderInputError = true;
+
+            return result;
+        }
+        private bool IsCurrentMemberSelectionBadlyFormatted(out string selectedEmail)
+        {
+            bool result = IsInputBadlyFormatted(CurrentMemberSelection, out selectedEmail);
+            if(result)
                 memberInputError = true;
+            
+            return result;
+        }
+        private bool IsInputBadlyFormatted(string input, out string email)                                   // method invoking it needs to set the style of the input
+        {
+            if (IsStringInMemberFormat(input) == false)
+            {
                 serverMessageIsError = true;
                 ServerMessage = "Please, enter the details in the reqired format.";
-                selectedEmail = "";
+                email = "";
                 return true;
             }
-            selectedEmail = GetEmailFromFormattedString(CurrentMemberSelection);
+            email = GetEmailFromFormattedString(input);
             return false;
         }
 
@@ -290,17 +308,31 @@ namespace EctBlazorApp.Client.Pages
             return false;
         }
 
-        private bool IsCurrentMemberSelectionEligible(string selectedEmail)
+        private bool IsCurrentLeaderSelectionIneligible(string selectedEmail)
         {
-            if (AvailableMembers.Any(a => a.Contains(selectedEmail)) == false)
-            {
+            bool result = IsEmailIneligibleForSelection(selectedEmail, AllAvailableLeaders);
+            if (result)
+                leaderInputError = true;
+
+            return result;
+        }
+        private bool IsCurrentMemberSelectionIneligible(string selectedEmail)
+        {
+            bool result = IsEmailIneligibleForSelection(selectedEmail, AvailableMembers);
+            if(result)
                 memberInputError = true;
+
+            return result;
+        }
+        private bool IsEmailIneligibleForSelection(string email, IEnumerable<string> list)
+        {
+            if (list.Any(a => a.Contains(email)) == false)
+            {
                 serverMessageIsError = true;
                 ServerMessage = "This user is not eligible for selection.";
                 return true;
             }
             return false;
         }
-
     }
 }

commit 61b30700d785d10ce077f65ce325963a85b85423

Fixing Manage Team bug #55 introduced with commit 3d6f5a2

src/Client/Pages/ManageTeam.razor
@@ -40,7 +40,7 @@
                             <label>Team lead</label>
                             <input id="createTeamLeader" list="leaders"
                                     @onchange="@((args) => SetLeaderNameEmail(args))"
-                                    placeholder="Full Name <email@example.com>" style="@LeaderInputStyle" />
+                                    placeholder="Full Name <email@example.com>" style="@LeaderInputStyle" disabled="@HasTeamId" />
                             <datalist id="leaders">
                                 @foreach (var leader in AvailableLeaders)
                                 {

src/Client/Pages/ManageTeam.razor.cs
@@ -317,6 +317,8 @@ namespace EctBlazorApp.Client.Pages
         }
         private bool IsCurrentLeaderSelectionIneligible(string selectedEmail)
         {
+            if (HasTeamId) return false;
+
             bool result = IsEmailIneligibleForSelection(selectedEmail, AllAvailableLeaders);
             if (result)
                 LeaderInputError = true;