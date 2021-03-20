dce99500758d2115b9c114b57c0f96b32a1c0a14
Pavel Ivanov <ppivanov98@gmail.com>
2021-03-20 16:41:03 +0000

    Adding delete method to ViewTeams #29

--- src/Client/Pages/ViewTeams.razor.cs
+++ src/Client/Pages/ViewTeams.razor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -5,6 +6,7 @@ using EctBlazorApp.Client.Graph;
 using EctBlazorApp.Client.Shared;
 using EctBlazorApp.Shared;
 using Microsoft.AspNetCore.Components;
+using static EctBlazorApp.Shared.SharedMethods;
 
 namespace EctBlazorApp.Client.Pages
 {
@@ -24,6 +26,29 @@ namespace EctBlazorApp.Client.Pages
         protected bool Initialized { get; set; } = false;
 
         protected EctTeamRequestDetails SelectedTeam { get; set; }
+
+        protected string ServerMessage { get; set; } = string.Empty;
+        protected bool ServerMessageIsError { get; set; } = false;
+
+
+        protected void DeleteSelectedTeam()
+        {
+            string hasedTeamId = ComputeSha256Hash(SelectedTeam.TeamId);
+            // bool success = await ApiConn.DeleteTeam(hasedTeamId);
+            // if (success) 
+            if(true)
+            {
+                ServerMessage = $"Successfully deleted {SelectedTeam.Name}";
+                ServerMessageIsError = false;
+                Teams = Teams.Where(t => t.TeamId != SelectedTeam.TeamId).ToList();
+                SelectedTeam = null;
+            }
+            else
+            {
+                ServerMessage = $"Failed to delete team. Please, try again later.";
+                ServerMessageIsError = true;
+            }
+        }
 
         protected void ExpandTeam(EctTeamRequestDetails team)
         {
