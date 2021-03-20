16438c78ef693a575b5ab8f5ac6407869cc7b7be
Pavel Ivanov <ppivanov98@gmail.com>
2021-03-20 14:55:43 +0000

    Displaying list of teams on ViewTeams #29

--- src/Client/Pages/ViewTeams.razor.cs
+++ src/Client/Pages/ViewTeams.razor.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using EctBlazorApp.Client.Graph;
 using EctBlazorApp.Client.Shared;
+using EctBlazorApp.Shared;
 using Microsoft.AspNetCore.Components;
 
 namespace EctBlazorApp.Client.Pages
@@ -14,10 +17,13 @@ namespace EctBlazorApp.Client.Pages
         [Inject]
         protected DashboardState DashboardState { get; set; }
 
+        protected List<EctTeamRequestDetails> Teams { get; set; } = new List<EctTeamRequestDetails>();
+
         protected override async Task OnInitializedAsync()
         {
             DashboardState.SetIsDrillDown(false);
             await CustomAuthState.GetUserPermissions(AuthState, ApiConn);
+            Teams = (await ApiConn.FetchAllTeams()).ToList();
         }
     }
 }
